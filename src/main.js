8c1b75245e075ba78caf3811480568e88f9998c4
stlwhj <4412453443@qq.com>
2019-08-01 21:44:57 +0800

    login页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from '@/router'
+import ElementUi from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUi)
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <el-card class="my-card">
+      <img src="../../assets/images/logo_index.png" alt />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="less">
+.container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+}
+.my-card {
+  width: 400px;
+  height: 350px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  img {
+    display: block;
+    width: 200px;
+    margin: 0 auto;
+  }
+}
+</style>
