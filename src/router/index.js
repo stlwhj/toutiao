75a4d6b240b7d6faaed0c3ed7855b832bec627f7
stlwhj <4412453443@qq.com>
2019-08-10 20:55:54 +0800

    素材管理完工

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Home from '@/views/home'
 
 import Welcome from '@/views/welcome'
 import Article from '@/views/article'
+import Image from '@/views/image'
 import NotFound from '@/views/404'
 import store from '@/store'
 Vue.use(VueRouter)
@@ -26,7 +27,8 @@ const router = new VueRouter(
           {
             path: '/', name: 'welcome', component: Welcome
           },
-          { path: '/article', name: 'article', component: Article }
+          { path: '/article', name: 'article', component: Article },
+          { path: '/image', name: 'image', component: Image }
         ]
       },
       {
