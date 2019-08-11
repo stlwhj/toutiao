d77de4927b180f78f91db2ed267e22d90f98f3c1
stlwhj <4412453443@qq.com>
2019-08-11 20:08:15 +0800

    发布文章完工

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,33 +1,42 @@
 <template>
-  <div class="container">
+  <div class="publish-container">
     <el-card>
       <div slot="header">
-        <my-bread>发布文章</my-bread>
+        <my-bread>{{articleId?'修改文章':'发布文章'}}</my-bread>
       </div>
       <el-form label-width="100px">
         <el-form-item label="标题：">
           <el-input v-model="articleForm.title" style="width:400px"></el-input>
         </el-form-item>
         <el-form-item label="内容：">
-          <quill-editor v-model="articleForm.content"></quill-editor>
+          <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
         </el-form-item>
         <el-form-item label="封面：">
-          <el-radio-group v-model="articleForm.cover.type">
+          <el-radio-group v-model="articleForm.cover.type" @change="changeType">
             <el-radio :label="1">单图</el-radio>
             <el-radio :label="3">三图</el-radio>
             <el-radio :label="0">无图</el-radio>
             <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
-          <div class="img-btn">
-            <img src="../../assets/images/default.png" alt />
+          <div v-if="articleForm.cover.type === 1">
+            <my-image v-model="articleForm.cover.images[0]"></my-image>
+          </div>
+          <div v-if="articleForm.cover.type === 3">
+            <my-image v-model="articleForm.cover.images[0]"></my-image>
+            <my-image v-model="articleForm.cover.images[1]"></my-image>
+            <my-image v-model="articleForm.cover.images[2]"></my-image>
           </div>
         </el-form-item>
         <el-form-item label="频道：">
-          <my-channel v-model=" articleForm.channel_id"></my-channel>
+          <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary">发表</el-button>
-          <el-button>存入草稿</el-button>
+        <el-form-item v-if="!articleId">
+          <el-button type="primary" @click="submit(false)">发表</el-button>
+          <el-button @click="submit(true)">存入草稿</el-button>
+        </el-form-item>
+        <el-form-item v-else>
+          <el-button type="success" @click="update(false)">修改</el-button>
+          <el-button @click="update(true)">存入草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -52,23 +61,71 @@ export default {
           images: []
         },
         channel_id: null
+      },
+      editorOption: {
+        placeholder: '',
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ indent: '-1' }, { indent: '+1' }],
+            ['image']
+          ]
+        }
+      },
+      articleId: null
+    }
+  },
+  watch: {
+    $route () {
+      if (!this.$route.query.id) {
+        this.articleId = null
+        this.articleForm = {
+          title: null,
+          content: null,
+          cover: {
+            type: 1,
+            images: []
+          },
+          channel_id: null
+        }
       }
     }
   },
-
-  methods: {}
+  created () {
+    this.articleId = this.$route.query.id
+    if (this.articleId) {
+      this.getArticle()
+    }
+  },
+  methods: {
+    async getArticle () {
+      const {
+        data: { data }
+      } = await this.$http.get('articles/' + this.articleId)
+      this.articleForm = data
+    },
+    changeType () {
+      this.articleForm.cover.images = []
+    },
+    async submit (draft) {
+      await this.$http.post(`articles?draft=${draft}`, this.articleForm)
+      this.$message.success(draft ? '文章存入草稿成功' : '文章发表成功')
+      this.$router.push('/article')
+    },
+    async update (draft) {
+      await this.$http.put(
+        `articles/${this.articleId}?draft=${draft}`,
+        this.articleForm
+      )
+      this.$message.success(draft ? '修改存入草稿成功' : '文章修改成功')
+      this.$router.push('/article')
+    }
+  }
 }
 </script>
 
 <style scoped lang='less' >
-.img-btn {
-  width: 160px;
-  height: 160px;
-  border: 1px dashed #ddd;
-  img {
-    width: 100%;
-    height: 100%;
-    display: block;
-  }
-}
 </style>
